bca0ccc122b03663690a225fc3a6119057547b51
Kristian Glass <git@doismellburning.co.uk>
2012-09-22 12:22:35 +0000

    Trivial setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='lict',
+    url='https://github.com/nhshackday/lict',
+    install_requires=[
+        'pyquery',
+        'neo4jrestclient',
+        'nltk',
+        'requests',
+    ],
+)
