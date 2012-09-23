df25b9f55f2f1db29d7481d3faac8e790b175249
Jeff Snyder <jeff-github@caffeinated.me.uk>
2012-09-23 15:44:50 +0000

    Scripts to generate mappings between articles & drug/mesh names
    mesh  = medical subject headings

---
+++ lict/lict/lib/drug_mapping.py
@@ -0,0 +1,30 @@
+#!/opt/nhshackday/wip/lict/bin/python -u
+# vim: set sw=4 sts=4 ts=4 et:
+import re
+from lxml import etree
+from pyquery import PyQuery as pq
+import urllib
+import subprocess
+import os
+import sys
+import drugs
+
+terms_fp = "/mnt/nhshackday/je4d/mesh-term-ids"
+#/mnt/nhshackday/je4d/medline-scrape/data
+if __name__ == "__main__":
+    dir = sys.argv[1]
+
+    druglist = drugs.druglist()
+    for dirname, dirnames, filenames in os.walk(sys.argv[1]):
+        for fn in map(lambda x: os.path.join(dirname, x), filter(lambda f:f.endswith(".nxml"), filenames)):
+            pmc = re.sub(".*/", "", fn.replace(".nxml",""))
+
+            f = open(fn, 'r')
+            fdata = f.read().lower()
+            dids = []
+
+            for did, dstr in druglist:
+                if fdata.find(dstr) != -1:
+                    dids.append(did)
+            if dids:
+                print "%s,%s" % (pmc, ','.join(dids))
