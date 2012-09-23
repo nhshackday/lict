a35dd3eafdfc6ff05bf12d373dd4e921a60dd446
Kristian Glass <git@doismellburning.co.uk>
2012-09-23 10:50:52 +0100

    requirements.txt for now

--- setup.py
+++ setup.py
@@ -3,12 +3,4 @@ from distutils.core import setup
 setup(
     name='lict',
     url='https://github.com/nhshackday/lict',
-    install_requires=[
-        'pyquery',
-        'neo4jrestclient',
-        'nltk',
-        'requests',
-        'numpy',
-        'django',
-    ],
 )
