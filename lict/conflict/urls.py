813532f300920c166f64925f096fc1129dd3af2c
Kristian Glass <git@doismellburning.co.uk>
2012-09-24 17:21:12 +0100

    Dummy handling of robots.txt and favicon.ico

--- lict/conflict/urls.py
+++ lict/conflict/urls.py
@@ -1,8 +1,11 @@
 # -*- coding: UTF-8 -*-
 from django.conf.urls import patterns, url
 from conflict.views import OrganisationListView, ArticleListView, ConflictListView, OrganisationDetailView
+from django.http import HttpResponse
 
 urlpatterns = patterns('conflict.views',
+    url(r'^robots\.txt', lambda r: HttpResponse('User-agent: *\nDisallow: /', mimetype='text/plain')),
+    url(r'^favicon\.ico', lambda r: HttpResponse('', mimetype='image/png')),
     url(r'^$', 'home', name='home'),
     url(r'^organisations/$', OrganisationListView.as_view(), name='organisations'),
     url(r'^organisations/(?P<pk>\d+)/$', OrganisationDetailView.as_view(), name='organisation'),
